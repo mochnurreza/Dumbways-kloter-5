3f40dc4b8cfb792a2ea6d7875b286d8a33f6a081
mochnurreza <57040391+mochnurreza@users.noreply.github.com>
2020-03-21 21:22:33 +0700

    Add files via upload

---
+++ alfanumerik.js
@@ -0,0 +1,8 @@
+function randomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+var rString = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+console.log(rString);
